2293412a14e09356b301509b81b693b49095e7ab
Yana Sacheva <ms.alissana@gmail.com>
2020-02-13 00:40:45 +0400

    Добавляет логику выбора комнат

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var ENTER_KEY = 'Enter';
 var COUNTCARDS = 8;
 var TYPEHOUSE = ['palace', 'flat', 'house', 'bungalo'];
 var TYPEHOUSE_LABELS = {
@@ -15,8 +16,12 @@ var TITLE = ['Уютная квартира на берегу залива', 'И
 var DESCRIPTION = ['Очень уютная квартира-студия в центре. Сделан евроремонт. Установлена вся необходимая техника: холодильник, свч, электрическая плита, посудомоечная, стиральная машина, телевизор, фен, утюг.', 'Квартира у Аквапарка Ривьера. С wi-fi кондиционером, горячая вода есть всегда! На кухне есть все необходимое что бы готовить и даже фартук.', 'Одноэтажная квартира (Лестница не используется и является частью интерьера) с отдельной спальней и совмещённой гостиной/кухней, отличный вариант как на пару, так и на компанию из 3-4 человек.', 'Удобное пространство, где могут разместиться до 3 человек. Эта квартира находится в 3 минутах езды от Синдзюку на поезде, а также недалеко от Сибуя!', 'Мой дом - типичный дом в японском стиле, расположенный в тихом жилом районе в 6 минутах ходьбы от станции Асагая.', 'Эти дизайнерские апартаменты находятся в 9 минутах ходьбы от станции MACHIYA.', 'TASU TOCO - это дизайнерская внутренняя квартира, расположенная в 15 минутах езды на поезде от станции Синдзюку.', 'Квартира находится в жилом районе, поэтому вы можете спать спокойно и спать по ночам.', 'У нас есть просторная кухня и лаундж, поэтому долгожданные гости очень приветствуются!'];
 
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
 var mapFilters = map.querySelector('.map__filters-container');
+var adForm = document.querySelector('.ad-form');
+var inputAdress = adForm.querySelector('#address');
+var adFormFieldset = adForm.querySelectorAll('fieldset');
+
+var mapFiltersForm = mapFilters.querySelector('.map__filters');
 
 var cardTemplate = document.querySelector('#card')
     .content;
@@ -24,6 +29,8 @@ var cardTemplate = document.querySelector('#card')
 var pinTemplate = document.querySelector('#pin')
     .content;
 var pinTemplateElement = document.querySelector('.map__pins');
+
+var mapPinMain = map.querySelector('.map__pin--main');
 
 function getRandElement(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
@@ -164,5 +171,108 @@ function renderPins(cards) {
 var cardsData = getCards(COUNTCARDS);
 var firstCard = [cardsData[0]];
 
-renderCards(firstCard);
-renderPins(cardsData);
+//renderCards(firstCard);
+
+function addAttribute(tagList, attributeName) {
+  if (tagList) {
+    for (var i = 0; i < tagList.length; i++) {
+      tagList[i].setAttribute(attributeName, 'true');
+    }
+  }
+}
+
+function deleteAttribute(tagList, attributeName) {
+  if (tagList) {
+    for (var i = 0; i < tagList.length; i++) {
+      tagList[i].removeAttribute(attributeName);
+    }
+  }
+}
+
+var disabled = 'disabled'
+addAttribute(adFormFieldset, disabled);
+
+
+function logButton(e, card) {
+  if (e.button === 0 || e.key === ENTER_KEY) {
+    map.classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+    mapFiltersForm.classList.remove('mapFiltersForm--disabled');
+    deleteAttribute(adFormFieldset, disabled);
+    renderPins(cardsData);
+  }
+}
+
+mapPinMain.addEventListener('mousedown', logButton);
+mapPinMain.addEventListener('keydown', logButton);
+
+/*
+  Рабочий вариант c изменением option
+*/
+
+// var rooms = adForm.querySelector('#room_number');
+// var guests = adForm.querySelector('#capacity');
+
+// var ROOMS_FOR_GUESTS = {
+//   '1': ['1'],
+//   '2': ['2', '1'],
+//   '3': ['3', '2', '1'],
+//   '100': ['0']
+// };
+
+// var CAPACITY = {
+//   '1': 'для 1 гостя',
+//   '2': 'для 2 гостей',
+//   '3': 'для 3 гостей',
+//   '0': 'не для гостей'
+// }
+
+// function validRoomsForGuests() {
+//   var roomValue = (typeof this !== 'undefined') ? this.value : 1;
+//   var validRooms = ROOMS_FOR_GUESTS[roomValue];
+//   var guestsHTML = '';
+
+//   for (var i = 0; i < validRooms.length; i++) {
+//     guestsHTML += '<option value="' + validRooms[i] + '">' + CAPACITY[validRooms[i]] + '</option>';
+//   }
+
+//   guests.innerHTML = guestsHTML;
+// }
+
+// rooms.addEventListener('change', validRoomsForGuests);
+
+// validRoomsForGuests();
+
+/*
+  Рабочий вариант без изменения option
+*/
+
+var rooms = adForm.querySelector('#room_number');
+var guests = adForm.querySelector('#capacity');
+
+var ROOMS_FOR_GUESTS = {
+  '1': ['1'],
+  '2': ['2', '1'],
+  '3': ['3', '2', '1'],
+  '100': ['0']
+};
+
+function validRoomsForGuests() {
+  var el = (typeof this !== 'undefined') ? this : rooms;
+  var validRooms = ROOMS_FOR_GUESTS[el.value];
+
+  var options = guests.querySelectorAll('option');
+
+  if (options) {
+    for (var i = 0; i < options.length; i++) {
+      var optionEl = options[i];
+
+      optionEl.disabled = (validRooms.indexOf(optionEl.value) === -1) ? true : false;
+    }
+
+    guests.querySelector('option[value="' + validRooms[0] + '"]').selected = true;
+  }
+}
+
+rooms.addEventListener('change', validRoomsForGuests);
+validRoomsForGuests();
